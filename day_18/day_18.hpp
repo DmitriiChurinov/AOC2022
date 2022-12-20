300ec8a086624fa9d658f8abf61e3d13ec74b812
DmitriiChurinov <churinovdmitriy@gmail.com>
2022-12-20 14:16:17 +0300

    day18: part2

--- day_18/day_18.hpp
+++ day_18/day_18.hpp
@@ -13,10 +13,10 @@ public:
         getDropsOfLavaFromFile(filename);
     }
 
-    int getDropsOfLavaSquare() {
+    int getDropsOfLavaSquare(bool needCheckAirPockets = false) {
+        lavaSquare.clear();
         for (auto item : dropsOfLava) {
-            if (!dropsOfLava.count({ item.x - 1, item.y, item.z }))
-            {
+            if (!dropsOfLava.count({ item.x - 1, item.y, item.z })) {
                 lavaSquare.insert({ item.x - 1, item.y, item.z });
             }
             if (!dropsOfLava.count({ item.x + 1, item.y, item.z })) {
@@ -35,7 +35,89 @@ public:
                 lavaSquare.insert({ item.x, item.y, item.z + 1 });
             }
         }
-        return lavaSquare.size();
+        int count = 0;
+        if (needCheckAirPockets) {
+            for (auto drop: lavaSquare) {
+                bool interest = true;
+                const int maxDistance = 30;
+                bool checkUpX = true;
+                bool checkDownX = true;
+                bool checkUpY = true;
+                bool checkDownY = true;
+                bool checkUpZ = true;
+                bool checkDownZ = true;
+                for (int i = 1; i < maxDistance && interest; i++) {
+                    if (checkUpX && dropsOfLava.count({ drop.x - i, drop.y, drop.z })) {
+                        checkUpX = false;
+                    }
+                    if (checkDownX && dropsOfLava.count({ drop.x + i, drop.y, drop.z })) {
+                        checkDownX = false;
+                    }
+                    if (checkUpY && dropsOfLava.count({ drop.x, drop.y - i, drop.z })) {
+                        checkUpY = false;
+                    }
+                    if (checkDownY && dropsOfLava.count({ drop.x, drop.y + i, drop.z })) {
+                        checkDownY = false;
+                    }
+                    if (checkUpZ && dropsOfLava.count({ drop.x, drop.y, drop.z - i})) {
+                        checkUpZ = false;
+                    }
+                    if (checkDownZ && dropsOfLava.count({ drop.x, drop.y, drop.z + i })) {
+                        checkDownZ = false;
+                    }
+                    if (checkUpX || checkDownX || checkUpY || checkDownY || checkUpZ || checkDownZ) {
+                        interest = true;
+                    }
+                    else {
+                        interest = false;
+                        vector<point> queue;
+                        const int maxCount = 2000;
+                        set<point> seen;
+                        seen.insert({ drop.x, drop.y, drop.z });;
+                        queue.push_back({ drop.x, drop.y, drop.z });
+                        while (!queue.empty() && seen.size() <= maxCount) {
+                            auto cur = queue[0];
+                            queue.erase(queue.begin());
+                            if (!dropsOfLava.count({ cur.x - 1, cur.y, cur.z }) && !seen.count({ cur.x - 1, cur.y, cur.z })) {
+                                seen.insert({ cur.x - 1, cur.y, cur.z });
+                                queue.push_back({ cur.x - 1, cur.y, cur.z });
+                            }
+                            if (!dropsOfLava.count({ cur.x + 1, cur.y, cur.z }) && !seen.count({ cur.x + 1, cur.y, cur.z })) {
+                                seen.insert({ cur.x + 1, cur.y, cur.z });
+                                queue.push_back({ cur.x + 1, cur.y, cur.z });
+                            }
+                            if (!dropsOfLava.count({ cur.x, cur.y - 1, cur.z }) && !seen.count({ cur.x, cur.y - 1, cur.z })) {
+                                seen.insert({ cur.x, cur.y - 1, cur.z });
+                                queue.push_back({ cur.x, cur.y - 1, cur.z });
+                            }
+                            if (!dropsOfLava.count({ cur.x, cur.y + 1, cur.z }) && !seen.count({ cur.x, cur.y + 1, cur.z })) {
+                                seen.insert({ cur.x, cur.y + 1, cur.z });
+                                queue.push_back({ cur.x, cur.y + 1, cur.z });
+                            }
+                            if (!dropsOfLava.count({ cur.x, cur.y, cur.z - 1 }) && !seen.count({ cur.x, cur.y, cur.z - 1 })) {
+                                seen.insert({ cur.x, cur.y, cur.z - 1 });
+                                queue.push_back({ cur.x, cur.y, cur.z - 1 });
+                            }
+                            if (!dropsOfLava.count({ cur.x, cur.y, cur.z + 1 }) && !seen.count({ cur.x, cur.y, cur.z + 1 })) {
+                                seen.insert({ cur.x, cur.y, cur.z + 1 });
+                                queue.push_back({ cur.x, cur.y, cur.z + 1 });
+                            }
+                        }
+                        if (seen.size() > maxCount) {
+                            count++;
+                        }
+                    }
+                }
+                if (interest) {
+                    count++;
+                }
+            }
+        }
+        else {
+            count = lavaSquare.size();
+        }
+
+        return count;
     }
 private:
     struct point {
@@ -62,5 +144,6 @@ void day18_start(string filename) {
     cout << endl << "Day 18" << endl;
     BoilingBoulders boilingBoulders(filename);
     cout << "part 1: " << boilingBoulders.getDropsOfLavaSquare() << endl;
-    cout << "part 2: " << endl;
+    //todo: подумать над ускорением
+    cout << "part 2: " << boilingBoulders.getDropsOfLavaSquare(true) << endl;
 }
